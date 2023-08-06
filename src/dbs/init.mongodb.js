2d92b1c1e36991314ae4a6d9b26fc672dedeacda
dat02092k1 <ngodat02092001@gmail.com>
2023-08-06 16:26:20 +0700

    "Create apiKey for system"

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -11,10 +11,10 @@ class Database {
 
     // connect
     connect(type = 'mongodb') {
-        // if (1 === 1) {
-        //     mongoose.set('debug', true);
-        //     mongoose.set('debug', { color: true }); 
-        // }
+        if (1 === 1) {
+            mongoose.set('debug', true);
+            mongoose.set('debug', { color: true }); 
+        }
 
         mongoose.connect(connectString, {
             maxPoolSize: 50
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const express = require('express');
+const { apiKey, permission } = require('../auth/checkAuth');
 
 const router = express.Router(); 
 
-router.use('/v1/api', require('./access'))
-// router.get('', (req, res, next) => {
+// check APIKey
+router.use(apiKey);
+// check permissions
+router.use(permission('0000'));  
 
-//     return res.status(200).json(
-//         { message: 'heh node' }
-//     );
-// })
+router.use('/v1/api', require('./access'))
 
 module.exports = router;
